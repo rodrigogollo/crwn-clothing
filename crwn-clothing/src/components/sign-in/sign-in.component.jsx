e602e8c085b0f386b323e6a1ec8b1815e0748aab
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2021-06-03 23:00:00 -0300

    creating signin page and signin components, form-input, custom-buttom and signin components

---
+++ crwn-clothing/src/components/sign-in/sign-in.component.jsx
@@ -0,0 +1,55 @@
+import React from 'react';
+import './sign-in.styles.scss';
+import FormInput from '../form-input/form-input.component.jsx';
+import CustomButton from '../../components/custom-button/custom-button.component.jsx';
+
+class SignIn extends React.Component {
+    constructor(props){
+        super(props);
+
+        this.state = {
+            email: '',
+            password: ''
+        }
+    }
+
+    handleSubmit = event => {
+        event.preventDefault();
+
+        this.setState({email: '', password: ''});
+    }
+
+    handleChange = event => {
+        const {name, value} = event.target;
+
+        this.setState({[name]: value});
+    }
+
+    render() {
+        return (
+            <div className="sign-in">
+                <h2>I Already Have an Account</h2>
+                <span>Sign in with your email and password</span>
+                <form onSubmit={this.handleSubmit}>
+                    <FormInput 
+                        name='email' 
+                        type='email' 
+                        value={this.state.email} 
+                        handleChange={this.handleChange}
+                        label="Email"
+                        required />
+                    <FormInput 
+                        name='password' 
+                        type='password' 
+                        value={this.state.password} 
+                        handleChange={this.handleChange}
+                        label="Password"
+                        required />
+                    <CustomButton type='submit'>Sign In</ CustomButton>
+                </form>
+            </div>
+        )
+    }
+}
+
+export default SignIn;
