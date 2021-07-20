36d0cba4f4f4ae39826c49e8a66f8aacf946a820
Rodrigo Gollo <rodrigogollo.nh@gmail.com>
2021-07-20 15:52:17 -0300

    added checkout page, checkout item component and updated cart to use selectors from reselect

--- crwn-clothing/src/App.js
+++ crwn-clothing/src/App.js
@@ -1,14 +1,20 @@
 import React from 'react';
 import { Switch, Route, Redirect } from 'react-router-dom';
 import { connect } from 'react-redux';
+import { createStructuredSelector } from 'reselect';
+
 import './App.css';
 
 import HomePage from './pages/homepage/homepage.component.jsx';
 import ShopPage from './pages/shop/shop.component.jsx';
 import SignInAndSignOutPage from './pages/sign-in-and-sign-up/sign-in-and-sign-up.component.jsx';
+import CheckoutPage from './pages/checkout/checkout.component';
+
 import Header from './components/header/header.component.jsx';
+
 import { auth, createUserProfileDocument } from './firebase/firebase.utils';
 import { setCurrentUser } from './redux/user/user.actions';
+import { selectCurrentUser } from './redux/user/user.selector';
 
 class App extends React.Component {
   unsubscribeFromAuth = null;
@@ -40,8 +46,9 @@ class App extends React.Component {
       <div>
         <Header />
           <Switch>
-            <Route exact path="/" component={ HomePage } />
-            <Route path="/shop" component={ ShopPage } />
+            <Route exact path="/" component={HomePage} />
+            <Route path="/shop" component={ShopPage} />
+            <Route exact path="/checkout" component={CheckoutPage} />
             <Route exact path="/signin" render={() => this.props.currentUser ? (<Redirect to="/" />): (<SignInAndSignOutPage />)} /> 
           </Switch>
       </div>
@@ -49,11 +56,9 @@ class App extends React.Component {
   }
 }
 
-const mapStateToProps = ({user}) => {
-  return {
-    currentUser: user.currentUser
-  }
-}
+const mapStateToProps = createStructuredSelector({
+  currentUser: selectCurrentUser
+})
 
 const mapDispatchToProps = (dispatch) => ({
   setCurrentUser: user => dispatch(setCurrentUser(user))
